9ed6071fb80d46bc9fc13f441a3950aef1484ac5
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2023-06-13 16:20:27 -0300

    requesito 1

---
+++ app/backend/src/database/models/SequelizeTeams.ts
@@ -0,0 +1,34 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class SequelizeTeams extends Model<InferAttributes<SequelizeTeams>,
+InferCreationAttributes<SequelizeTeams>> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+SequelizeTeams.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'Teams',
+  timestamps: false,
+});
+
+export default SequelizeTeams;
